8edda22623d258de91fe33f215b4664b329db3af
Alex Babenko <alex0128.94@mail.ru>
2020-07-17 17:38:02 +0300

    #8 GraphQL: Retrieving data from Masterdata

--- node/index.ts
+++ node/index.ts
@@ -1,55 +1,56 @@
-import {
-  LRUCache,
-  Service,
-  ServiceContext,
-  ParamsContext,
-  RecorderState,
-  method,
-} from '@vtex/api'
-import { Clients } from './clients'
-import { analytics } from './handlers/analytics'
-import { updateLiveUsers } from './event/liveUsersUpdate'
+import {LRUCache, method, ParamsContext, RecorderState, Service, ServiceContext,} from '@vtex/api'
+import {Clients} from './clients'
+import {analytics} from './handlers/analytics'
+import {updateLiveUsers} from './event/liveUsersUpdate'
+import {productList} from "./resolvers/products";
 
 // Create a LRU memory cache for the Status client.
 // The @vtex/api HttpClient respects Cache-Control headers and uses the provided cache.
-const memoryCache = new LRUCache<string, any>({ max: 5000 })
+const memoryCache = new LRUCache<string, any>({max: 5000})
 metrics.trackCache('status', memoryCache)
 
 declare global {
-  type Context = ServiceContext<Clients, State>
+    type Context = ServiceContext<Clients, State>
 
-  interface State extends RecorderState {
-    code: number
-  }
+    interface State extends RecorderState {
+        code: number
+    }
 }
 
 const TREE_SECONDS_MS = 3 * 1000
 const CONCURRENCY = 10
 
 export default new Service<Clients, State, ParamsContext>({
-  clients: {
-    implementation: Clients,
-    options: {
-      default: {
-        retries: 2,
-        timeout: 10000,
-      },
-      events: {
-        exponentialTimeoutCoefficient: 2,
-        exponentialBackoffCoefficient: 2,
-        initialBackoffDelay: 50,
-        retries: 1,
-        timeout: TREE_SECONDS_MS,
-        concurrency: CONCURRENCY,
-      },
+    clients: {
+        implementation: Clients,
+        options: {
+            default: {
+                retries: 2,
+                timeout: 10000,
+            },
+            events: {
+                exponentialTimeoutCoefficient: 2,
+                exponentialBackoffCoefficient: 2,
+                initialBackoffDelay: 50,
+                retries: 1,
+                timeout: TREE_SECONDS_MS,
+                concurrency: CONCURRENCY,
+            },
+        },
     },
-  },
-  routes: {
-    analytics: method({
-      GET: [analytics],
-    }),
-  },
-  events: {
-    liveUsersUpdate: updateLiveUsers,
-  },
+    routes: {
+        analytics: method({
+            GET: [analytics],
+        }),
+    },
+    events: {
+        liveUsersUpdate: updateLiveUsers,
+    },
+    graphql: {
+        resolvers: {
+            Query: {
+                productList,
+            },
+        },
+    },
 })
